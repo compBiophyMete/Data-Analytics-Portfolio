73adf30abb95e7cf85e5dcf1af6d717927ef78db
compBiophyMete <135632077+compBiophyMete@users.noreply.github.com>
2023-12-24 21:55:38 +0100

    Add files via upload

--- Project-8: Investigation of Employees in Coffeeshops using SQL/extraction.sql
+++ Project-8: Investigation of Employees in Coffeeshops using SQL/extraction.sql
@@ -1,12 +1,12 @@
--- S1: Extract employees with a salary of > 50,000 USD.
-SELECT * FROM employees WHERE salary > 50000;
+-- S1: Extract employees with a salary of > 20,000 USD.
+SELECT * FROM employees WHERE salary > 20000;
 -- S2: Extract only the employees working in Common Grounds Coffeeshop.
 SELECT * FROM employees WHERE coffeeshop_id = 1;
--- S3: Extract only the employees working in Common Grounds Coffeeshop and making a salary of  > 50,000 USD.
-SELECT * FROM employees WHERE coffeeshop_id = 1 and salary > 50000;
--- S4: Extract only the male employees working in Common Grounds CoffeeShop, making a salary of > 50,000 USD.
-SELECT * FROM employees WHERE coffeeshop_id = 1 AND salary > 50000 AND gender = 'M';
--- S5: Extract all the employees who work in Common Grounds or make more than 50k or are male.
+-- S3: Extract only the employees working in Common Grounds Coffeeshop and making a salary of  > 30,000 USD.
+SELECT * FROM employees WHERE coffeeshop_id = 1 and salary > 30000;
+-- S4: Extract only the male employees working in Common Grounds CoffeeShop, making a salary of > 40,000 USD.
+SELECT * FROM employees WHERE coffeeshop_id = 1 AND salary > 40000 AND gender = 'M';
+-- S5: Extract all the employees who work in Common Grounds or make more than 50,000 USD or are male.
 SELECT * FROM employees WHERE coffeeshop_id = 1 OR salary > 50000 OR gender = 'M';
 -- S6: Extract all rows from the suppliers table where the supplier is Beans and Barley.
 SELECT * FROM suppliers WHERE supplier_name = 'Beans and Barley';
@@ -19,12 +19,12 @@ SELECT * FROM suppliers WHERE NOT coffee_type IN ('Robusta', 'Arabica');
 SELECT * FROM employees WHERE email IS NULL;
 -- S10: Extract all employees whose emails are not missing.
 SELECT * FROM employees WHERE NOT email is NULL;
--- S11: Extract all employees who make between 35,000 and 50,000 USD.
-SELECT * FROM employees WHERE salary BETWEEN 35000 AND 50000;
+-- S11: Extract all employees who make between 25,000 and 55,000 USD.
+SELECT * FROM employees WHERE salary BETWEEN 25000 AND 55000;
 -- S12: Extract employee_id, first_name, and last_name sorted by salary in descending order.
 SELECT employee_id, first_name, last_name, salary FROM employees ORDER BY salary DESC;
--- S13: Find the top 10 highest paid employees.
-SELECT * FROM employees ORDER BY salary DESC LIMIT 10;
+-- S13: Find the top 5 highest paid employees.
+SELECT * FROM employees ORDER BY salary DESC LIMIT 5;
 -- S14: Extract  all unique coffeeshop ids in the employees table.
 SELECT DISTINCT coffeeshop_id FROM employees;
 -- S15: Extract year, month, and day of hire
@@ -34,25 +34,25 @@ EXTRACT (DAY from hire_date) as day
 FROM employees;
 -- S16: Concatenate first and last names to create full names.
 SELECT first_name, last_name, first_name || ' ' || last_name as full_name FROM employees;
--- S17:	Extract the full name of the employees. If a person makes < 50,000 USD, then true; otherwise, false.
-SELECT first_name || ' '  || last_name as full_name, salary < 50000 as less_than_50k
+-- S17:	Extract the full name of the employees. If a person makes < 20,000 USD, then true; otherwise, false.
+SELECT first_name || ' '  || last_name as full_name, salary < 20000 as less_than_20k
 FROM employees ;
--- S18:	Extract the full name of the employees. If a person is a female and makes less than 50,000 USD, then true.
+-- S18:	Extract the full name of the employees. If a person is a female and makes less than 60,000 USD, then true.
 SELECT first_name || ' ' || last_name as full_name,
-(gender = 'F' AND salary < 50000) as less_than_50k_female FROM employees;
+(gender = 'F' AND salary < 60000) as less_than_50k_female FROM employees;
 -- S19: If an email has '.com', return true, otherwise false.
 SELECT email, email LIKE '%.com%' FROM employees;
 -- S20: Extract the full name of the only government employees.
 SELECT first_name || ' ' || last_name as full_name FROM employees WHERE email LIKE '%.gov%';
 
--- S21: Return the email addresses beginning from the 5th character.
-SELECT email, SUBSTRING(email FROM 5) FROM employees;
+-- S21: Return the email addresses beginning from the 2nd character.
+SELECT email, SUBSTRING(email FROM 2) FROM employees;
 -- S22: Find the position of the '@'.
 SELECT email, POSITION('@' IN email) as at_pos FROM employees;
 -- S23: Find the email addresses of the employees via a substring.
 SELECT email, SUBSTRING(email FROM POSITION('@' IN email)+1) FROM employees;
--- S24: Fill the null emails as NO EMAIL PROVIDED.
-SELECT email, COALESCE(email, 'NO EMAIL PROVIDED') FROM employees;
+-- S24: Fill the null emails as MISSING EMAIL INFO.
+SELECT email, COALESCE(email, 'MISSING EMAIL INFO') FROM employees;
 
 -- S25: Find the minimum salary.
 SELECT MIN(salary) as min_salary FROM employees;
@@ -74,29 +74,29 @@ SELECT coffeeshop_id, SUM(salary) as total_salary FROM employees GROUP BY coffee
 -- S33: Find the number of employees and the average, minimum, maximum, and total salaries for each coffeeshop.
 SELECT coffeeshop_id, COUNT(employee_id), AVG(salary) as avg_salary, MIN(salary) as min_salary,
 MAX(salary) as max_salary, SUM(salary) as total_salary FROM employees GROUP BY coffeeshop_id;
--- S34: Find the number of employees and the average, minimum, maximum, and total salaries of coffeeshops with more than 200 employees, where coffeeshops are sorted in descending order by number of employees.
+-- S34: Find the number of employees and the average, minimum, maximum, and total salaries of coffeeshops with more than 100 employees, where coffeeshops are sorted in descending order by number of employees.
 SELECT coffeeshop_id, COUNT(employee_id) as nuOfEmp, AVG(salary) as avg_salary, MIN(salary) as min_salary,
 MAX(salary) as max_salary, SUM(salary) as sum_salary FROM employees GROUP BY coffeeshop_id
-HAVING COUNT(employee_id) > 200 ORDER BY COUNT(employee_id) DESC;
--- S35: Extract only the coffeeshops with a minimum salary of less than 10,000 USD.
+HAVING COUNT(employee_id) > 100 ORDER BY COUNT(employee_id) DESC;
+-- S35: Extract only the coffeeshops with a minimum salary of less than 5,000 USD.
 SELECT coffeeshop_id, COUNT(employee_id) as nuOfEmp, AVG(salary) as avg_salary, MIN(salary) as min_salary,
 MAX(salary) as max_salary, SUM(salary) as sum_salary FROM employees GROUP BY coffeeshop_id
-HAVING MIN(salary) < 10000 ORDER BY COUNT(employee_id) DESC;
--- S36: Return employee_id, full_name, salary ,and pay_category, i.e high pay or low pay If pay is less than 50,000 USD, then low pay, otherwise high pay. Sort the return by salaries in descending order.
+HAVING MIN(salary) < 5000 ORDER BY COUNT(employee_id) DESC;
+-- S36: Return employee_id, full_name, salary ,and pay_category, i.e high pay or low pay If pay is less than 10,000 USD, then low pay, otherwise high pay. Sort the return by salaries in descending order.
 SELECT employee_id, first_name || ' ' || last_name as full_name, salary,
 CASE
-WHEN salary < 50000 THEN 'low pay'
+WHEN salary < 10000 THEN 'low pay'
 ELSE 'high pay'
 END as pay_category
 FROM employees ORDER BY salary DESC;
 
--- S37: If pay is less than 20k, then low pay
--- if between 20k-50k inclusive, then medium pay
--- if over 50k, then high pay
+-- S37: If pay is less than 10k, then low pay
+-- if between 10k-40k inclusive, then medium pay
+-- if over 40k, then high pay
 SELECT employee_id, first_name || ' ' || last_name as full_name, salary,
 CASE
-WHEN salary < 20000 THEN 'low pay'
-WHEN salary > 20000 AND salary < 50000 THEN 'medium pay'
+WHEN salary < 10000 THEN 'low pay'
+WHEN salary > 10000 AND salary < 40000 THEN 'medium pay'
 ELSE 'high pay'
 END as pay_category
 FROM employees ORDER BY salary DESC;
@@ -104,8 +104,8 @@ FROM employees ORDER BY salary DESC;
 SELECT pC.pay_category, COUNT(*) FROM (
 SELECT employee_id, first_name || ' ' || last_name as full_name, salary,
 	CASE
-	WHEN salary < 20000 THEN 'low pay'
-	WHEN salary > 20000 AND salary < 50000 THEN 'medium pay'
+	WHEN salary < 10000 THEN 'low pay'
+	WHEN salary > 10000 AND salary < 40000 THEN 'medium pay'
 	ELSE 'high pay'
 	END as pay_category
 FROM employees ORDER BY salary DESC
@@ -113,9 +113,9 @@ FROM employees ORDER BY salary DESC
 GROUP BY pC.pay_category;
 -- S39: Tranposed form of the returned list above.
 SELECT
-	SUM(CASE WHEN salary < 20000 THEN 1 ELSE 0 END) AS low_pay,
-	SUM(CASE WHEN salary BETWEEN 20000 AND 50000 THEN 1 ELSE 0 END) AS medium_pay,
-	SUM(CASE WHEN salary > 50000 THEN 1 ELSE 0 END) AS high_pay
+	SUM(CASE WHEN salary < 10000 THEN 1 ELSE 0 END) AS low_pay,
+	SUM(CASE WHEN salary BETWEEN 10000 AND 40000 THEN 1 ELSE 0 END) AS medium_pay,
+	SUM(CASE WHEN salary > 40000 THEN 1 ELSE 0 END) AS high_pay
 FROM employees;
 -- S40: INNER JOIN of shops table with locations table.
 INSERT INTO locations VALUES (4, 'Paris', 'France');
@@ -181,8 +181,8 @@ SELECT * FROM shops WHERE city_id IN (SELECT city_id FROM locations WHERE city_i
 -- S53: Return all employees who work in US coffeeshops.
 SELECT * FROM employees WHERE coffeeshop_id IN (SELECT coffeeshop_id FROM shops WHERE city_id IN (
 SELECT city_id FROM locations WHERE country = 'United States'));
--- S54: Return all employees who make over 35,000 USD and work in US coffeeshops.
-SELECT * from employees WHERE salary > 35000 AND coffeeshop_id IN
+-- S54: Return all employees who make over 15,000 USD and work in US coffeeshops.
+SELECT * from employees WHERE salary > 15000 AND coffeeshop_id IN
 (SELECT coffeeshop_id FROM shops WHERE city_id IN (SELECT city_id FROM locations
 WHERE country = 'United States'));
 -- S55: Return 30-day moving total pay.
